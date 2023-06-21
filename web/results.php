9dddaf3a38ddb6b6a678e2b4e51722d9c2c16911
Lech Szymanski <lech.szymanski@otago.ac.nz>
2023-06-21 14:47:29 +1200

    Collage

--- web/results.php
+++ web/results.php
@@ -3,7 +3,8 @@
     $gen_folder = "generated";
     $cmd_folder = "cmd";
 
-    $results_count = count(glob($gen_folder . "/demo*"));
+	$results = glob($gen_folder . "/demo*");
+    $results_count = count($results);
 
 
 ?>
@@ -64,6 +65,9 @@
 					<img id="img_s1" src="" height="vh">
 				</div>
 
+				<div id="show2">
+
+				</div>				
 
 			</div>
 		</div>
@@ -75,11 +79,27 @@
 
 	var ready = true;
 	var results_count = <?php echo $results_count; ?>;
+	let results = <?php echo json_encode($results); ?>;
 	var last_shown = -1;
 	let switch_to_other_show = 5;
 	var count_to_switch_to_other_show = switch_to_other_show;
-	let num_shows = 1;
+	let num_shows = 2;
 	var index_show = 0;
+	var img_cache = [];
+	let collage_rows = 4;
+	var collage_cols;
+	var collage_size;
+
+	for(var i=0; i<results.length; i++) {
+		img_cache.push(results[i]);
+	}
+
+	function pad(num, size) {
+    	num = num.toString();
+    	while (num.length < size) num = "0" + num;
+   		return num;
+	}
+
 
 	function sleep(ms) {
 		return new Promise(resolve => setTimeout(resolve, ms));
@@ -90,9 +110,13 @@
 		var done = false;
 		var step = 0;
 		var n_iter = 1;
+		var n_available;
+		var n_idx;
+		var delay = 50;
 
 		if(s==0) {
 			$("#show1").hide();
+			$("#show2").hide();
 			$("#img_s1").css("visibility", "hidden");
 			$("#show0_title").show();
 			$("#show0").show();			
@@ -102,7 +126,33 @@
 			$("#show0").hide();
 			$("#show0_title").hide();
 			$(".img_s0").css("visibility", "hidden");
+			$("#show2").hide();
 			$("#show1").show();
+		} else if(s==2) {
+			set_collage_vars();
+			$("#show0").hide();
+			$("#show0_title").hide();
+			$(".img_s0").css("visibility", "hidden");
+			$("#show1").hide();
+			$("#show2").show();
+			step = 'final';
+
+			let num_ims = collage_cols*collage_rows;
+			var index_perm = permute(num_ims);
+			var available = [];
+			for(let i=0;i<num_ims;i++) {
+				for(let j=1;j<=3;j++) {
+					available.push([i,j]);
+				}
+			}
+			available = permute_array(available);
+
+			n_available = available.pop();
+			n_idx = index_perm.pop();
+
+			msg.demo = "demo" + pad(n_available[0], 5);
+			n_iter = n_available[1];
+			delay = 200;
 		}
 
 
@@ -124,6 +174,9 @@
 							var img_id = '#img_s0_' + n_iter;
 							if(s==1) {
 								img_id = '#img_s1';
+							} else if(s==2) {
+								img_id = n_idx;
+								img_id = '#img_s2_' + (img_id);
 							}
 							$(img_id).load(umsg.im_file, function(responseTxt, statusTxt, xhr) {
 								if(statusTxt == "success") {
@@ -133,11 +186,30 @@
 								}
 							});
 							if(umsg.final) {
-								if(umsg.n_iter >= parseInt(msg.n_iter) || s==1) {
-									done = true;
+								if(s < 2) {
+									if(umsg.n_iter >= parseInt(msg.n_iter) || s==1) {
+										done = true;
+									} else {
+										n_iter = umsg.n_iter + 1;
+										step = 0;
+									}
+									if(img_cache.includes(umsg.im_file)) {
+										im_cache.push(umsg.im_file);
+									}
 								} else {
-									n_iter = umsg.n_iter + 1;
-									step = 0;
+									if(available.length > 0 && index_perm.length > 0) { 
+
+										n_available = available.pop();
+										n_idx = index_perm.pop();
+										msg.demo = "demo" + pad(n_available[0], 5);
+										n_iter = n_available[1];
+										next = true;
+										if(delay > 20) {
+											delay -= 20;
+										}
+									} else {
+										done = true;
+									}
 								}
 							} else if (umsg.step != 'final') {
 								step = umsg.step + 1;
@@ -146,6 +218,15 @@
 								}
 							}
 						} else {
+							if(s== 2) {
+								if(available.length > 0) { 
+									n_available = available.pop();
+									msg.demo = "demo" + pad(n_available[0], 5);
+									n_iter = n_available[1];
+								} else {
+									done = true;
+								}
+							}
 							next=true;
 						}
 					},
@@ -153,7 +234,7 @@
 						next=true;
 					}    
 			});    
-			await sleep(50);
+			await sleep(delay);
 		}
 		await sleep(10000);
 		ready = true;
@@ -167,10 +248,17 @@
             success: async function( msg ) {
                 msg = JSON.parse(msg);
                 if(msg.ok) {
+					if(!results.includes(msg.demo)) {
+						results.push(msg.demo);
+					}
+
 					if(msg.rcount > results_count) {
 						check_update(msg, 0);
 					} else {
 						if(count_to_switch_to_other_show <= 0) {
+							if(index_show == 1 && (results.length*3) < (collage_cols*collage_rows)) {
+								index_show = 0;
+							}
 							check_update(msg, index_show+1);
 							index_show = (index_show + 1) % num_shows;
 							count_to_switch_to_other_show = switch_to_other_show;
@@ -204,9 +292,62 @@
 		
 	}
 
+	function permute(max) {
+	  
+	  indices = [];
+	  for(let i=0;i<max;i++) {
+		indices.push(i);
+	  }
+
+	  for (let i = 0; i < max; i++) {
+    	j = Math.floor(Math.random() * max);
+		temp = indices[i];
+		indices[i] = indices[j];
+		indices[j] = temp;
+	  }
+
+	  return indices;	  
+	}
+
+	function permute_array(array) {
+
+		indices = permute(array.length);
+
+		var perm_array = [];
+
+		for(let i=0;i<array.length;i++) {
+			perm_array.push(array[indices[i]]);
+		}
+		return perm_array;
+	}
+
+	function set_sh2() {
+		$(".img_s2").remove();
+
+		var k = 0;
+		for(var r=0; r<collage_rows; r++) {
+			for(var c=0; c<collage_cols; c++) {
+				var img_id = 'img_s2_' + k;
+				var img = $('<img id="' + img_id + '" class="img_s2" src="" width="' + collage_size + '" height="' + collage_size + '">');
+				k += 1;
+				$("#show2").append(img);
+			}
+		}
+		$(".img_s2").css("visibility","hidden");
+
+	}
+
+	function set_collage_vars() {
+		collage_size = Math.floor($(window).height() / collage_rows);
+		collage_cols = Math.floor($(window).width() / collage_size);
+		set_sh2();
+	}
+
 
 	$( document ).ready(function() {
  
+		set_collage_vars();
+	
 		$(window).on('resize', function(){
 			$("#img_s1").height($(this). height());
 		});
